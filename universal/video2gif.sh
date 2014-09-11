6ff327d9bc1d60f55295e3c840946cde65a85c09
Tom Himanen <tom.himanen@gmail.com>
2014-09-12 01:18:01 +0300

    video2gif doesn't pee anywhere anymore

--- universal/video2gif.sh
+++ universal/video2gif.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir .frames
-ffmpeg -i $1 -vf scale=320:-1 -r 20 .frames/ffout%03d.png
-convert -delay 5 -loop 0 .frames/ffout*.png $2 
+mkdir .frames &&
+ffmpeg -i $1 -vf scale=480:-1 -r 20 .frames/ffout%03d.png &&
+cp .frames/ffout020.png $2.png &&
+convert -delay 5 -loop 0 .frames/ffout*.png $2 &&
 rm -rf .frames
